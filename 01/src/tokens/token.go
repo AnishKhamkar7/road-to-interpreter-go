ca0d6077747d5dfd147a67a8f8aa5ec62ec1191a
AnishKhamkar <117926974+AnishKhamkar7@users.noreply.github.com>
2026-03-13 10:03:24 +0530

    added INT  const in the Token

--- 01/src/tokens/token.go
+++ 01/src/tokens/token.go
@@ -3,31 +3,25 @@ package tokens
 type TokenType string
 
 type Token struct {
-
 	Type TokenType
 
 	Literal string
 }
 
 const (
-
-	ILLEGAL = "Illegal" 
-	EOF = "EOF"
-	
-	ASSIGN = "="
-	PLUS = "+"
-
-	IDENT = "IDENT"
-
-	COMMA = ","
+	ILLEGAL   = "Illegal"
+	EOF       = "EOF"
+	ASSIGN    = "="
+	PLUS      = "+"
+	IDENT     = "IDENT"
+	//identifier
+	INT       = "INT"
+	COMMA     = ","
 	SEMICOLON = ";"
-	LPAREN = "("
-	RPAREN = ")"
-	LBRACE = "}"
-	RBRACE = "{"
-
-	FUNCTION = "FUNCTION"
-	LET = "LET"
-	
-
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "}"
+	RBRACE    = "{"
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
 )
